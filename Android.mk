922c38e77276535d94563e13afa8cac8c52f37b7
Nagender Telkar <ntelkar@codeaurora.org>
2012-01-19 10:11:44 -0700

    display: Build dependencies for framework libs
    Build these libs for emulator/sdk as well because
    surfaceflinger includes these libs
    
    Change-Id: I05c30095e8beb651550e75930185c5ce6f6eed57
    CRs-fixed: 326753

--- Android.mk
+++ Android.mk
@@ -1,6 +1,11 @@
 #Enables the listed display HAL modules
+
+#Libs to be built for all targets (including SDK)
+display-hals := libqcomui libtilerenderer
+
+#libs to be built for QCOM targets only
 ifeq ($(call is-vendor-board-platform,QCOM),true)
-display-hals := libhwcomposer liboverlay libgralloc libgenlock libcopybit libtilerenderer
-display-hals += libqcomui
-include $(call all-named-subdir-makefiles,$(display-hals))
+display-hals += libhwcomposer liboverlay libgralloc libgenlock libcopybit
 endif
+
+include $(call all-named-subdir-makefiles,$(display-hals))
--- libqcomui/Android.mk
+++ libqcomui/Android.mk
@@ -7,12 +7,18 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
         libutils \
         libcutils \
-        libmemalloc \
         libui \
         libEGL
 
 LOCAL_C_INCLUDES := $(TOP)/hardware/qcom/display/libgralloc \
 LOCAL_CFLAGS := -DLOG_TAG=\"libQcomUI\"
+
+ifneq ($(call is-vendor-board-platform,QCOM),true)
+    LOCAL_CFLAGS += -DNON_QCOM_TARGET
+else
+    LOCAL_SHARED_LIBRARIES += libmemalloc
+endif
+
 LOCAL_MODULE := libQcomUI
 LOCAL_MODULE_TAGS := optional
 include $(BUILD_SHARED_LIBRARY)
